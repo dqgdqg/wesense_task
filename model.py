9fd24416ce5d8f5304ab8fabb146330254e66618
dqgdqg <qgding@gmail.com>
2022-10-04 17:26:29 -0400

    Modify hidden units from 26 to 32.

--- model.py
+++ model.py
@@ -10,19 +10,19 @@ class Net(nn.Module):
         self.bn = bn
         self.dropout = dropout
 
-        self.linear_1 = nn.Linear(26, 26)
+        self.linear_1 = nn.Linear(26, 32)
         self.dropout_1 = nn.Dropout(0.5)
-        self.bn_1 = nn.BatchNorm1d(26)
+        self.bn_1 = nn.BatchNorm1d(32)
 
-        self.linear_2 = nn.Linear(26, 26)
+        self.linear_2 = nn.Linear(32, 32)
         self.dropout_2 = nn.Dropout(0.5)
-        self.bn_2 = nn.BatchNorm1d(26)
+        self.bn_2 = nn.BatchNorm1d(32)
 
-        self.linear_3 = nn.Linear(26, 26)
+        self.linear_3 = nn.Linear(32, 32)
         self.dropout_3 = nn.Dropout(0.5)
-        self.bn_3 = nn.BatchNorm1d(26)
+        self.bn_3 = nn.BatchNorm1d(32)
 
-        self.out = nn.Linear(26, 7)
+        self.out = nn.Linear(32, 7)
         
 
     def forward(self, x):
